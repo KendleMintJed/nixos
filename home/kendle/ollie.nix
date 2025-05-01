c0be5f891b4f916f2e4c12e794f19d67feb96eed
KendleMintJed <KendleMintJed@proton.me>
2025-05-01 19:03:47 +0100

    Configure git commit signing

--- home/kendle/ollie.nix
+++ home/kendle/ollie.nix
@@ -34,12 +34,14 @@ in {
       credential.helper = "store";
       core.editor = "nvim";
       init.defaultBranch = "main";
+      commit.gpgsign = true;
       user = {
         # Otherwise git will try to guess a default user identity. see `man git config`.
         useConfigOnly = true;
 
         name = "KendleMintJed";
-        email = "jamie.astonfamily@gmail.com";
+        email = "KendleMintJed@proton.me";
+        signingkey = "0C0AE68B63FA8F1757B97F9E31BB390EC1A9FAB5";
       };
     };
   };
