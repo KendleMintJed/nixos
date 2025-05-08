a2905871a258eb1d1aea74856d8f14f227f84b17
KendleMintJed <KendleMintJed@proton.me>
2025-05-08 23:36:29 +0100

    Fix some formatting

--- hosts/nixos/ollie/default.nix
+++ hosts/nixos/ollie/default.nix
@@ -20,14 +20,16 @@
   xdg.portal.enable = true;
   xdg.portal.extraPortals = with pkgs; [xdg-desktop-portal-gtk];
 
-  services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
 
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
 
-  services.xserver.xkb = {
-    layout = "gb";
-    variant = "";
+    xkb = {
+      layout = "gb";
+      variant = "";
+    };
   };
 
   console.keyMap = "uk";
