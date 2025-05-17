b9002d3f4c823db330a5408a67028d8b0a4ce8ef
KendleMintJed <KendleMintJed@proton.me>
2025-05-17 23:40:54 +0100

    Minor theming changes

--- home/kendle/common/core/qt.nix
+++ home/kendle/common/core/qt.nix
@@ -1,6 +1,15 @@
-{...}: {
+{pkgs, ...}: let
+  theme = "Adwaita-Dark";
+in {
   qt = {
     enable = true;
     platformTheme.name = "gtk";
+
+    style.name = theme;
+  };
+
+  home.sessionVariables = {
+    QT_STYLE_OVERRIDE = theme;
+    QT_QUICK_CONTROLS_STYLE = theme;
   };
 }
--- hosts/common/core/xdg.nix
+++ hosts/common/core/xdg.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   xdg.portal.enable = true;
   xdg.portal.extraPortals = with pkgs; [
+    xdg-desktop-portal
     xdg-desktop-portal-gtk
     xdg-desktop-portal-hyprland
   ];
