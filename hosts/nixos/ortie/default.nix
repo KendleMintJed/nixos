bdc1cefdcac385af887196ceeb0da0c0d9beb272
KendleMintJed <KendleMintJed@proton.me>
2025-06-10 05:01:45 +0100

    Add ortie machine

---
+++ hosts/nixos/ortie/default.nix
@@ -0,0 +1,78 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = lib.flatten [
+    ./hardware-configuration.nix
+
+    {nixpkgs.overlays = [inputs.hyprpanel.overlay];}
+
+    (map lib.custom.relativeToRoot [
+      "hosts/common/core"
+    ])
+  ];
+
+  boot.loader.grub.useOSProber = true;
+
+  networking = {
+    hostName = "ortie";
+    networkmanager.enable = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+  };
+
+  services.printing.enable = true;
+
+  security.rtkit.enable = true;
+
+  users.users.kendle = {
+    isNormalUser = true;
+    description = "kendle";
+    extraGroups = ["networkmanager" "wheel"];
+    shell = pkgs.zsh;
+  };
+
+  home-manager = {
+    backupFileExtension = "backup";
+    extraSpecialArgs = {inherit inputs;};
+    users = {
+      "kendle" = import (lib.custom.relativeToRoot "home/kendle/ortie.nix");
+    };
+  };
+
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamemode.enable = true;
+
+  programs.git.enable = true;
+
+  programs.yubikey-touch-detector.enable = true;
+
+  programs.ssh.startAgent = false;
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    mullvad-browser
+    yubioath-flutter
+    yubikey-manager
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
+  system.stateVersion = "25.05";
+}
