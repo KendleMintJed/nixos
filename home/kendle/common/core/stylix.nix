a3fcc724f75a13d7939924b435e4791e1fb8477e
KendleMintJed <KendleMintJed@proton.me>
2025-06-10 14:36:01 +0100

    Enable qt on stylix and update flake
    Stylix was broken due to flake being out of date

--- home/kendle/common/core/stylix.nix
+++ home/kendle/common/core/stylix.nix
@@ -1,13 +1,7 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
-  # imports = [inputs.stylix.homeManagerModules.stylix];
+{...}: {
   stylix = {
     enable = true;
     autoEnable = false;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     targets = {
       bat.enable = true;
       btop.enable = true;
--- hosts/common/core/stylix.nix
+++ hosts/common/core/stylix.nix
@@ -10,7 +10,7 @@
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     targets = {
       gtk.enable = true;
-      #qt.enable = true;
+      qt.enable = true;
     };
   };
 }
