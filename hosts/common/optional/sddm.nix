7e4f811e976b5472ff982c23119127caf94304b0
KendleMintJed <KendleMintJed@proton.me>
2025-10-15 03:30:13 +0100

    Fix sddm

--- hosts/common/optional/sddm.nix
+++ hosts/common/optional/sddm.nix
@@ -1,19 +1,19 @@
 {
   pkgs,
-  pkgs-fix,
   lib,
   ...
 }: {
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "catppuccin-mocha";
+    theme = "catppuccin-mocha-blue";
     package = pkgs.kdePackages.sddm;
   };
 
-  environment.systemPackages = with pkgs-fix; [
+  environment.systemPackages = with pkgs; [
     (catppuccin-sddm.override {
       flavor = "mocha";
+      accent = "blue";
       font = "JetBrainsMono Nerd Font";
       loginBackground = true;
       background = lib.custom.relativeToRoot "media/wallpaper.png";
