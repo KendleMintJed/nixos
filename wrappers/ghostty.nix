f65abf8a1d50074b65a4df1585b3fbe6d8765c9a
KendleMintJed <KendleMintJed@proton.me>
2026-08-21 20:56:13 +0100

    Ghostty opacity 0.5 -> 0.8

--- wrappers/ghostty.nix
+++ wrappers/ghostty.nix
@@ -8,7 +8,7 @@
         font-family = "JetBrainsMono Nerd Font";
         theme = "Catppuccin Mocha";
         window-decoration = "none";
-        background-opacity = 0.5;
+        background-opacity = 0.8;
         confirm-close-surface = false;
       };
     };
